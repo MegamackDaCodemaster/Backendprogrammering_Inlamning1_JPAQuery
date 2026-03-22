60ae89769afe200b97e7922c39d484c504c7fb90
MegamackDaCodemaster <ac75115@skola.goteborg.s>
2026-03-22 16:22:44 +0100

    Lade till EntityManagerFactory '(motorn i JPA)' och EntityManager '(Databas-session)'

--- src/main/java/se/yrgo/util/JpaUtil.java
+++ src/main/java/se/yrgo/util/JpaUtil.java
@@ -1,4 +1,17 @@
 package se.yrgo.util;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+// En klass som ansvarar för kopplingen till derby-databasen
+
 public class JpaUtil {
+
+    private static final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("jpa-unit");
+
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
 }
