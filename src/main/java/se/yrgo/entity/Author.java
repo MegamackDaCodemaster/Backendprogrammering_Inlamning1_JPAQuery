9ed6d6b76f306c72f10ffec614e91e22c1ccd9b4
MegamackDaCodemaster <ac75115@skola.goteborg.s>
2026-03-22 16:35:44 +0100

    Lade till mallen som används för att skapa Author-objekt

--- src/main/java/se/yrgo/entity/Author.java
+++ src/main/java/se/yrgo/entity/Author.java
@@ -1,4 +1,40 @@
 package se.yrgo.entity;
 
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+// En entitets-klass som motsvarar tabellen (Author) i databasen
+
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String nationality;
+
+    @OneToMany
+    private List<Book> books = new ArrayList<>();
+
+    public Author() {}
+
+    public Author(String name, String nationality) {
+        this.name = name;
+        this.nationality = nationality;
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
