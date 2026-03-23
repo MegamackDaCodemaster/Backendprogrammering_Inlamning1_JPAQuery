9b1f76564662f96f369d6dc09c92bd6926994a9c
MegamackDaCodemaster <ac75115@skola.goteborg.s>
2026-03-23 13:59:47 +0100

    En mall för att skapa Reader-objekt har lagts till

--- src/main/java/se/yrgo/entity/Reader.java
+++ src/main/java/se/yrgo/entity/Reader.java
@@ -1,4 +1,36 @@
 package se.yrgo.entity;
 
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+// Entitetsklass som mappas till tabellen "Reader" i databasen
+
+@Entity
 public class Reader {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @ManyToMany
+    private List<Book> books = new ArrayList<>();
+
+    public Reader() {}
+
+    public Reader(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public String getName() {
+        return name;
+    }
 }
