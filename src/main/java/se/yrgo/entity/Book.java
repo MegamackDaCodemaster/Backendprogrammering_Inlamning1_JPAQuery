ade81e9b7b829fd28f484dc6d17a46f1cf19878c
MegamackDaCodemaster <ac75115@skola.goteborg.s>
2026-03-23 13:49:10 +0100

    En 'NamedQuery' och mall för att skapa bok-objekt har lagts till

--- src/main/java/se/yrgo/entity/Book.java
+++ src/main/java/se/yrgo/entity/Book.java
@@ -1,4 +1,38 @@
 package se.yrgo.entity;
 
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+// En klass som skapar bok-objekt, mappas till tabellen "Book" i databasen
+
+@Entity
+@NamedQuery(
+        name = "Book.findByGenre",
+        query = "SELECT b FROM Book b WHERE b.genre = :genre"
+)
 public class Book {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+    private String genre;
+    private int publicationYear;
+
+    @ManyToMany(mappedBy = "books")
+    private List<Reader> readers = new ArrayList<>();
+
+    public Book() {}
+
+    public Book(String title, String genre, int year) {
+        this.title = title;
+        this.genre = genre;
+        this.publicationYear = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
